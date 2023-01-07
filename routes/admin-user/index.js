9531c979a1066a22af3561a16058998cc3dd9b22
Petar <petar@MacBook-Pro.local>
2023-01-07 20:01:32 +0000

    Changing cookie settings

--- routes/admin-user/index.js
+++ routes/admin-user/index.js
@@ -50,10 +50,9 @@ app.put("/users/login", function (req, res) {
                     // in the browser from accessing the cookie data, which will act as protection from potential
                     // cross-site scripting XSS attacks
                     httpOnly: true,
-                    sameSite: 'None',
                     // marks the cookie to be used with HTTPS only. This should only be applied in production mode, otherwise
                     // testing things in development mode would be impossible (where HTTP is used)
-                    secure: process.env.NODE_ENV === "production",
+                    // secure: process.env.NODE_ENV === "production",
                     // the method used for cookie value encoding. In this case the cookie is a String data type. 
                     // This would normally default to encodeURLComponent
                     encode: String,
