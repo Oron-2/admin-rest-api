27ec49bebf38c6b7098252b3674aeb9b5405460c
Petar <petar@MacBook-Pro.local>
2023-01-09 06:05:01 +0000

    Changes to the rest-api/routes/admin-user/index.js file

--- routes/admin-user/index.js
+++ routes/admin-user/index.js
@@ -1,13 +1,16 @@
 // This file is where all the individual routes for the admin user API endpoints are built. 
+const express = require("express")
+const tldjs = require("tldjs")
 
-const express = require("express");
-const tldjs = require("tldjs");
-
-const api = require("./api.js");
-
-const config = require("../../config.js");
+const api = require("./api.js")
 
 const authAdminUser = require("../../middlewares/index.js").authAdminUser
+
+const config = require("../../config.js")
+
+// express.Router() creates a new router object
+// It is an Express instance that will be used as middleware in the application
+const app = express.Router()
 
 /*
 // Calling the createNewAdminUser function decalred inside the /routes/admin-user/api.js file with actual parameters
@@ -15,10 +18,6 @@ api.createNewAdminUser("PetarPandzharov@protonmail.ch", "1234567", function (api
     console.log(apiResponse);
 })
 */
-
-// express.Router() creates a new router object
-// It is an Express instance that will be used as middleware in the application
-const app = express.Router();
 
 // A middleware function that have access to the request and response objects 
 // It applies to the /users/login path
@@ -29,42 +28,19 @@ app.put("/users/login", function (req, res) {
     if (!req.body.email || !req.body.password) {
         res.json({ success: false })
     } else {
-        // If req.body.email and req.body password are present, the logAdminUser() function is called
-        // that wil compare the two values against what's in the database. That function will reside in the 
-        // /routes/admin-user/api.js file
         api.loginAdminUser(req.body.email, req.body.password, function (apiResponse) {
             if (!apiResponse.success) {
                 res.json({ success: false })
             } else {
-                // If the email and password match what's in the database, a new auth token and expiration date
-                // were set in the database. Those values should be stored in the browser's cookies so the admin user
-                // can be authenticated without having to login again.
                 const cookieSettings = {
-                    // the URL path for what page the cookie data belongs to. By setting this to "/", it will work for all
-                    // pages on the website
                     path: "/",
-                    // the expiration date of the cookie in GMT formatted time. This should match the authentication token
-                    // expiration date stored in the database
                     expires: new Date(apiResponse.authTokenExpiresTimestamp * 1000),
-                    // flags the cookie to only be accessible by the admin REST API application. This restricts JavaScript
-                    // in the browser from accessing the cookie data, which will act as protection from potential
-                    // cross-site scripting XSS attacks
                     httpOnly: true,
-                    sameSite: "none",
-                    // marks the cookie to be used with HTTPS only. This should only be applied in production mode, otherwise
-                    // testing things in development mode would be impossible (where HTTP is used)
                     secure: process.env.NODE_ENV === "production",
-                    // the method used for cookie value encoding. In this case the cookie is a String data type. 
-                    // This would normally default to encodeURLComponent
                     encode: String,
-                    // the domain name assigned to the cookie. This must exactly match the domain of the admin website,
-                    // otherwise the cookie won't work. The tldjs NPM package is used to dynamically extract 
-                    // the domain name from the production admin URL (stored in the config.js file)
                     domain: process.env.NODE_ENV === "production" ? tldjs.parse(config.prodAdminURL).domain : ""
                 }
-                // To do the above, the res.cookie() Express method was used to attach the cookie data to the response that 
-                // is sent back to the admin website. The cookie have a name of adminUser and include a string value of 
-                // your admin user's id and auth token combined and separated by the & symbol. 
+
                 res.cookie("adminUser", apiResponse.userId + "&" + apiResponse.authToken, cookieSettings)
 
                 res.json({ success: true })
@@ -121,4 +97,4 @@ app.put("/users/change-password", authAdminUser, function (req, res) {
     }
 })
 
-module.exports = app;
+module.exports = app
